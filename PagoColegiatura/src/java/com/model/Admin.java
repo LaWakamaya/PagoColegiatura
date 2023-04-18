aa120924591e53cfbfe79301efd0a4a6df107493
Alan Franco <alanfrancoj@gmail.com>
2023-04-18 14:47:58 -0600

    Clases modelo
    Agregadas las clases para manejar los objetos con la informacion necesaria.

---
+++ PagoColegiatura/src/java/com/model/Admin.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+/**
+ *
+ * @author Alan Franco
+ */
+public class Admin {
+    
+    private String usuario, nombre, pass;
+
+    public Admin(String usuario, String nombre, String pass) {
+        this.usuario = usuario;
+        this.nombre = nombre;
+        this.pass = pass;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+    
+    
+}
---
+++ PagoColegiatura/src/java/com/model/Transaccion.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+/**
+ *
+ * @author Alan Franco
+ */
+public class Transaccion {
+    
+    private int id, matricula;
+    private String nombre, fecha, tipopago;
+    private double monto;
+
+    public Transaccion(int id, int matricula, String nombre, String fecha, String tipopago, double monto) {
+        this.id = id;
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.tipopago = tipopago;
+        this.monto = monto;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipopago() {
+        return tipopago;
+    }
+
+    public void setTipopago(String tipopago) {
+        this.tipopago = tipopago;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+    
+    
+    
+}
